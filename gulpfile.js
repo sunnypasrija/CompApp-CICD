d836087ad86f14e12dd81b44edbd3c2141b28fb1
in8sunpa <49863671+in8sunpa@users.noreply.github.com>
2020-07-28 10:51:23 +0200

    modified

--- gulpfile.js
+++ gulpfile.js
@@ -4,4 +4,29 @@ const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
+var settingsJSON = require('./config/envSettings.json')
+
+let configCopy = build.subTask('configCopy', (gulp, buildOptions, done) => {
+
+    if (settingsJSON.environment == "Ring1"){
+        gulp.src('./environment_specific/Ring1/serve.json')
+        .pipe(gulp.dest('./config/'));
+      }
+              
+      else {
+        
+        gulp.src('./environment_specific/All/serve.json')
+        .pipe(gulp.dest('./config/'));
+
+            }
+      
+            // Don't forget to tell SPFx you are done
+      done();
+  
+  })
+  
+  // register task to prebuild process
+  build.rig.addPreBuildTask(configCopy);
+
+
 build.initialize(require('gulp'));
