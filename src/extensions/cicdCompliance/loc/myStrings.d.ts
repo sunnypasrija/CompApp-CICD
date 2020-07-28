ba3e010c221a9d37ca6cacf59168ea06a88bd0c4
in8sunpa <49863671+in8sunpa@users.noreply.github.com>
2020-07-28 07:31:37 +0200

    initial commit

---
+++ src/extensions/cicdCompliance/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ICicdComplianceApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'CicdComplianceApplicationCustomizerStrings' {
+  const strings: ICicdComplianceApplicationCustomizerStrings;
+  export = strings;
+}
